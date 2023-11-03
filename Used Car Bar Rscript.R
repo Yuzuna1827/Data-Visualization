78d77a6b62821ce9fd5ec90b4b9d44b75f70327f
Yuzuna1827 <149146975+Yuzuna1827@users.noreply.github.com>
2023-11-03 14:45:42 +0800

    Add files via upload

---
+++ Used Car Bar Rscript.R
@@ -0,0 +1,25 @@
+library("ggplot2")
+df_used_cars <- read.csv("/Users/a0000/Desktop/Data Vis/bar_chart_data.csv",
+                         header = TRUE,
+                         sep = ",")
+
+bar_chart <- ggplot(df_used_cars,
+                    aes(x = Brand,
+                        y = Cars.Listings))+
+                        geom_bar(stat = "identity",
+                                 width = 0.8,
+                                 color= "navy",
+                                 fill = "navy")+
+  ggtitle("Cars Listings by Brand")+
+  theme_classic()+
+  theme(axis.text.x = element_text(angle = 45,
+                                   hjust = 1))+
+  labs(x = NULL,
+       y = "Number of listings")
+                            
+bar_chart
+
+
+
+
+
