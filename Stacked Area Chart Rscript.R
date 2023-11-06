273c8089d8f6166dfdd032ff35efebfed4ff1ed5
Yuzuna1827 <149146975+Yuzuna1827@users.noreply.github.com>
2023-11-06 09:25:19 +0800

    Add files via upload

---
+++ Stacked Area Chart Rscript.R
@@ -0,0 +1,29 @@
+library ("ggplot2")
+library ("dplyr")
+library("reshape2")
+
+df_engine_types <- read.csv("/Users/a0000/Desktop/Data Vis/Stacked Area Chart/stacked_area_chart_data.csv",
+                            header = TRUE,
+                            sep = ",")
+
+temp <- select(df_engine_types, -matches("Other"))
+new_engine_fuel_types <- melt(temp,id.vars = "Year")
+area_chart <- ggplot (new_engine_fuel_types,
+                      aes(x = Year,
+                          y = value,
+                          fill = variable))+
+              geom_area()+
+  scale_fill_manual(values = c("#ef2026","#7e2987","#011638"))+
+  theme_classic()+
+  theme(legend.justification = c(0.01,1),
+        legend.position = c(0.01,1))+
+  labs(fill = "Engine Type")+
+  ylab("Number of Cars")+
+  ggtitle("Popularity of Engine Fuel Types (1982 - 2016)")+
+  
+  theme(axis.text.x = element_text(angle = 90,
+                                   vjust = 0.5))+
+  scale_x_continuous(breaks = new_engine_fuel_types$Year)
+ 
+  
+area_chart
